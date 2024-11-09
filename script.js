e754364b399f0037818ff1db6a829e7996d5cd47
Yaric <iiiokehj@gmail.com>
2024-11-09 19:08:33 +0200

    delete dots.png

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
     function fetchTopicsCount() {
-        fetch('https://server-odq6bxtui-iokehjs-projects.vercel.app/api/topics/count')
+        fetch('https://server-hw6ax8jnj-iokehjs-projects.vercel.app/api/topics/count')
             .then(response => response.json())
             .then(data => {
                 const postsCountElement = document.getElementById('posts-count');
-                postsCountElement.textContent = `Створено постів: ${data.count}`;
+                postsCountElement.textContent = `Створено постів: ${data.length}`;
+                const ListThread = document.getElementById('Posts');
+                data.forEach((item) => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `
+                    <td>${item.name}</td>
+                    <td>${item.postsCount}</td>
+                    <td>${item.lastMessage}</td> `;
+                    ListThread.appendChild(row);
+                });
+                console.log(data);
             })
             .catch((error) => console.error('Error fetching topics count:', error) )
     }
     function fetchPostsCount() {
-        fetch('https://server-odq6bxtui-iokehjs-projects.vercel.app/api/messages/count')
+        fetch('https://server-hw6ax8jnj-iokehjs-projects.vercel.app/api/messages/count')
             .then(response => response.json())
             .then(data => {
                 const postsCountElement = document.getElementById('message');
