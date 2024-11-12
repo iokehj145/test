b185f588c5d0bd6766cc031c58198e5ce7da18b1
Yaric <iiiokehj@gmail.com>
2024-11-12 17:27:52 +0200

    check auth 4

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
         login.addEventListener('click', LoginFun);
     }
     function fetchTopicsCount() {
-        fetch('https://server-logkq3lgh-iokehjs-projects.vercel.app/api/topics/count')
+        fetch('https://server-8eqc8v1ej-iokehjs-projects.vercel.app/api/topics/count')
             .then(response => response.json())
             .then(data => {try {
                 const postsCountElement = document.getElementById('posts-count');
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }).catch((error) => console.error('Error fetching topics count:', error) )
     }
     function fetchPostsCount() {
-        fetch('https://server-logkq3lgh-iokehjs-projects.vercel.app/api/messages/count')
+        fetch('https://server-8eqc8v1ej-iokehjs-projects.vercel.app/api/messages/count')
             .then(response => response.json())
             .then(data => {
                 const postsCountElement = document.getElementById('message');
