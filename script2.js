9e90b9bd5186883f216c78a376959dda371895eb
Yaric <iiiokehj@gmail.com>
2024-11-16 12:43:40 +0200

    selector

--- script2.js
+++ script2.js
@@ -2,6 +2,19 @@ const formatDate = (isoDate) => {
     const date = new Date(isoDate);
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
 };
+const filterComments = (value) => {
+    if (value === 'new')
+        ListOfMessange.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+    else
+        ListOfMessange.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+    console.log(ListOfMessange);
+    document.querySelectorAll('.comment').forEach((section) => section.remove());
+    ListOfMessange.forEach((iteam) => {
+        const message = new Message(iteam._id, iteam.numberid, iteam.userEmail, iteam.message, iteam.createdAt);
+        const commentSection = document.querySelector('.comment-section-main');
+        commentSection.appendChild(message.render());
+    });
+}
 class Message {
     constructor(id, numberid, email, text, createdAt) {
         this.id = id;
@@ -24,13 +37,14 @@ class Message {
         return newComment;
     }
 }
+let ListOfMessange;
 document.addEventListener('keydown', async (e) => {
     const message = document.getElementById('comment_input');
     if (e.key === 'Enter' && message.value !== "") {
         const text = message.value;
         message.value += "...";
         message.disabled = true;
-        const response = await fetch('https://server-mckx5ngi3-iokehjs-projects.vercel.app/topics/'+localStorage.getItem('id')+'/posts', {
+        const response = await fetch('https://server-q43ekriiy-iokehjs-projects.vercel.app/topics/'+localStorage.getItem('id')+'/posts', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -48,17 +62,17 @@ document.addEventListener('keydown', async (e) => {
         else console.error(data.message);
     }
   });
-let ListOfMessange;
 document.addEventListener("DOMContentLoaded", function() {
     function fetchMessang() {
-        fetch('https://server-mckx5ngi3-iokehjs-projects.vercel.app/topics/'+localStorage.getItem('id')+'/posts')
+        fetch('https://server-q43ekriiy-iokehjs-projects.vercel.app/topics/'+localStorage.getItem('id')+'/posts')
             .then(response => response.json())
             .then(data => {
-                const commentSection = document.querySelector('.comment-section');
+                const commentSection = document.querySelector('.comment-section-main');
                 data.forEach((iteam) => {
                 const message = new Message(iteam._id, iteam.numberid, iteam.userEmail, iteam.message, iteam.createdAt);
                 commentSection.appendChild(message.render());
                 });
+                console.log(data);
                 ListOfMessange = data;
             })
             .catch((error) => console.error('Error fetching topics count:', error) )
